8ecf225c1a62983d3a2f3b0e48d0edd5c92faace
shd00700 <64176753+shd00700@users.noreply.github.com>
2020-06-08 16:34:53 +0900

    Update Pollprogram.go

--- Pollprogram.go
+++ Pollprogram.go
@@ -6,17 +6,26 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"strconv"
-	      //"reflect"
+	"strings"
+
+	//"reflect"
 	//"strings"
-	"time"
+
 )
+func Scrclr(){
+	cmd := exec.Command("cmd", "/c", "cls")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+
+}
 
 //MBClient config
 type MBClient struct {
 	IP      string
 	Port    int
-	Timeout time.Duration
+
 	Conn    net.Conn
 }
 
@@ -29,11 +38,11 @@ const (
 )
 
 // NewClient creates a new Modbus Client config.
-func NewClient(IP string, port int, timeout time.Duration ) *MBClient {
+func NewClient(IP string, port int) *MBClient {
 	m := &MBClient{}
 	m.IP = IP
 	m.Port = port
-	m.Timeout = timeout
+
 
 	return m
 }
@@ -53,6 +62,7 @@ func (m *MBClient) Open() error {
 
 //Close modbus tcp connetion
 func (m *MBClient) Close() {
+	os.Exit(12)
 	if m.Conn != nil {
 		m.Conn.Close()
 	}
@@ -67,7 +77,7 @@ func (m *MBClient) IsConnected() bool {
 }
 
 //Qurry make a modbus tcp qurry
-func Qurry(conn net.Conn, timeout time.Duration, pdu []byte) ([]byte, error) {
+func Qurry(conn net.Conn, pdu []byte) ([]byte, error) {
 	if conn == nil {
 		return []byte{}, fmt.Errorf(Disconnect)
 	}
@@ -81,7 +91,7 @@ func Qurry(conn net.Conn, timeout time.Duration, pdu []byte) ([]byte, error) {
 
 	//read
 	rbuf := make([]byte, 1024)
-	conn.SetReadDeadline(time.Now().Add(timeout))
+	//conn.SetReadDeadline(time.Now().Add(timeout))
 	len, err := conn.Read(rbuf)
 	if err != nil {
 		return nil, fmt.Errorf(Disconnect)
@@ -93,26 +103,27 @@ func Qurry(conn net.Conn, timeout time.Duration, pdu []byte) ([]byte, error) {
 }
 
 //ReadCoil mdbus function 1 qurry and return []uint16
-func (m *MBClient) ReadCoil(id uint8, addr uint16, leng uint16) ([]bool, error) {
+func (m *MBClient) ReadCoil(id uint8, addr uint16, leng uint16) ([]int, error) {
 	pdu := []byte{id, 0x01, byte(addr >> 8), byte(addr), byte(leng >> 8), byte(leng)}
 
-	res, err := Qurry(m.Conn, m.Timeout, pdu)
+	res, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
-		return []bool{}, err
+		return []int{}, err
 	}
 	//convert
-	result := []bool{}
+	result := []int{}
 	bc := res[2]
 	for i := 0; i < int(bc); i++ {
 		for j := 0; j < 8; j++ {
 			if (res[3+i] & (byte(1) << byte(j))) != 0 {
-				result = append(result, true)
+				result = append(result, 1)
 			} else {
-				result = append(result, false)
+				result = append(result, 0)
 			}
 		}
 	}
@@ -121,29 +132,30 @@ func (m *MBClient) ReadCoil(id uint8, addr uint16, leng uint16) ([]bool, error) 
 }
 
 //ReadCoilIn mdbus function 2 qurry and return []uint16
-func (m *MBClient) ReadCoilIn(id uint8, addr uint16, leng uint16) ([]bool, error) {
+func (m *MBClient) ReadCoilIn(id uint8, addr uint16, leng uint16) ([]int, error) {
 
 	pdu := []byte{id, 0x02, byte(addr >> 8), byte(addr), byte(leng >> 8), byte(leng)}
 
 	//write
-	res, err := Qurry(m.Conn, m.Timeout, pdu)
+	res, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
-		return []bool{}, err
+		return []int{}, err
 	}
 
 	//convert
-	result := []bool{}
+	result := []int{}
 	bc := res[2]
 	for i := 0; i < int(bc); i++ {
 		for j := 0; j < 8; j++ {
 			if (res[3+i] & (byte(1) << byte(j))) != 0 {
-				result = append(result, true)
+				result = append(result, 1)
 			} else {
-				result = append(result, false)
+				result = append(result, 0)
 			}
 		}
 	}
@@ -158,9 +170,10 @@ func (m *MBClient) ReadReg(id uint8, addr uint16, leng uint16) ([]uint16, error)
 	pdu := []byte{id, 0x03, byte(addr >> 8), byte(addr), byte(leng >> 8), byte(leng)}
 
 	//write
-	res, err := Qurry(m.Conn, m.Timeout, pdu)
+	res, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
@@ -184,9 +197,10 @@ func (m *MBClient) ReadRegIn(id uint8, addr uint16, leng uint16) ([]uint16, erro
 	pdu := []byte{id, 0x04, byte(addr >> 8), byte(addr), byte(leng >> 8), byte(leng)}
 
 	//write
-	res, err := Qurry(m.Conn, m.Timeout, pdu)
+	res, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
@@ -216,9 +230,10 @@ func (m *MBClient) WriteCoil(id uint8, addr uint16, data bool) error {
 	}
 
 	//write
-	_, err := Qurry(m.Conn, m.Timeout, pdu)
+	_, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
@@ -234,9 +249,10 @@ func (m *MBClient) WriteReg(id uint8, addr uint16, data uint16) error {
 	pdu := []byte{id, 0x06, byte(addr >> 8), byte(addr), byte(data >> 8), byte(data)}
 
 	//write
-	_, err := Qurry(m.Conn, m.Timeout, pdu)
+	_, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
@@ -256,7 +272,9 @@ func (m *MBClient) WriteCoils(id uint8, addr uint16, data []string) error {
 	}
 	var tbuf byte
 	for i := 0; i < len(data); i++ {
-		pb, _ := strconv.ParseBool(data[i]);
+		pb, _ := strconv.ParseBool(data[i])
+		pa,_ := strconv.Atoi(data[i])
+		fmt.Println("alias  ", addr, ": ", pa)
 		if pb {
 			tbuf |= byte(1 << uint(i%8))
 		}
@@ -265,12 +283,13 @@ func (m *MBClient) WriteCoils(id uint8, addr uint16, data []string) error {
 			pdu = append(pdu, tbuf)
 			tbuf = 0
 		}
+		addr++
 	}
-
 	//write
-	_, err := Qurry(m.Conn, m.Timeout, pdu)
+	_, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
 		}
@@ -282,187 +301,299 @@ func (m *MBClient) WriteCoils(id uint8, addr uint16, data []string) error {
 
 
 func (m *MBClient) WriteRegs(id uint8, addr uint16, data []string)  error {
-	fmt.Println("id:",id,""," addr:",addr,"")
 
 
 	//var data []byte
 	pdu := []byte{id, 0x10, byte(addr >> 8), byte(addr), byte(len(data) >> 8), byte(len(data)), byte(len(data)) * 2}
 	for i := 0; i < len(data); i++ {
-		pi, _ := strconv.ParseUint(data[i], 10, 16);
-		if pi == 0{
-			fmt.Println("")
-			main()
-		}
-		fmt.Println("alias  ",i, ": ", pi)
+		pi, _ := strconv.ParseUint(data[i], 10, 16)
+		fmt.Println("alias  ",addr, ": ", pi)
 		pdu = append(pdu, byte(pi>>8))
 		pdu = append(pdu, byte(pi))
 		//fmt.Println(pdu)
+		addr++
 	}
-	println("안녕하세용")
+
 
 	//write
-	_, err := Qurry(m.Conn, m.Timeout, pdu)
+	_, err := Qurry(m.Conn, pdu)
 	if err != nil {
 		if err.Error() == Disconnect {
+			fmt.Println("\n\n\n\n\n\n@@@Disconnect error@@@\n\n\n\n\n\n")
 			m.Close()
 			m.Conn = nil
+
+
 		}
 		return err
 	}
 	return nil
 }
 
+func Error(){
+	println("-------------------------------------")
+	fmt.Println("error")
+	println("-------------------------------------")
+	fmt.Println("You entered it iolncorrectly.\nReturn to the Output Coils menu.")
+	println("-------------------------------------\n\n\n")
+	fmt.Print("1 : Back\n","2 : Main menu\n\n\n")
+	fmt.Print("Select number Enter:")
+}
+func Continue(){
+	println("\n\n\n-------------------------------------")
+	fmt.Println("1 : Back","\n2 : Main menu")
+	println("-------------------------------------\n\n\n")
+	fmt.Print("Select number Enter:")
+}
+
 func main() {
 	//var arr []string
 	var num int
-	var alias uint16
+	var add uint16
 	var leng uint16
 	//TCP coonnetion
-	mbc := NewClient("192.168.0.222", 502, time.Second)
-
-	err := mbc.Open()
+	mbc := NewClient("192.168.0.222", 502)
+	mbc.Open()
+	/*err := mbc.Open()
 	if err != nil {
-		log.Panicln(err)
-		os.Exit(12)
-	}
+		log.Println("disconnect",err)
+
+	}*/
 
 	defer mbc.Close()
 
 	data, _ := mbc.ReadReg(1, 0, 10)
 	log.Println(data)
+
 	input := bufio.NewScanner(os.Stdin)
-loop :
+Main:
 	for {
-		fmt.Print("\n\n\n\n\n\n##Modbus protocol##\n")
+		Scrclr()
+		println("-------------------------------------")
+		fmt.Println("Main Menu")
+		println("-------------------------------------")
 		fmt.Print("1 : Output Coils\n","2 : Input Coils\n","3 : Input Registers\n","4 : Holding Registers\n\n\n\n\n")
 		fmt.Print("Select number Enter:")
-		fmt.Scanln(&num,"\n\n\n\n\n\n")
+		fmt.Scanln(&num)
+
 		if num == 1{
-		loop2:
+			OutputCoils:
 			for {
-				fmt.Print("\n\nno.1    : Write Coils \n","no.2    : Read Coils\n","Any key : Go back\n\n\n")
+				Scrclr()
+				println("-------------------------------------")
+				fmt.Println("Output Coils")
+				println("-------------------------------------")
+				fmt.Print("1 : Write Coils\n","2 : Read Coils\n","3 : Go back\n\n\n")
 				fmt.Print("Select number Enter:")
 				fmt.Scanln(&num)
-				if num == 1{
-					var arr []string
-					fmt.Println("\n\n\n\n@@Output Coils@@\nWrite Coils\n\n\n")
-					fmt.Print("\nalias values: ")
-					fmt.Scanln(&alias)
-					fmt.Print("leng values:")
-					fmt.Scanln(&leng)
-					for i := 0; i < int(leng); i++ {
-						fmt.Print("Enter data values: ")
-						input.Scan()
-						err, _ := strconv.Atoi(input.Text())
-						if err == 1 || err == 0{
-							arr = append(arr,input.Text())
-							mbc.WriteCoils(1, alias, arr)
-						} else{
-							fmt.Println("\n\n\nPlease enter again")
-							continue loop2
-						}
-					}
 
+				if num == 1{
+
+					Scrclr()
+					var arr []string
+					var leng int
+
+					println("-------------------------------------")
+					fmt.Println("Write Coils")
+					println("-------------------------------------")
+					fmt.Print("\n\n\nStart address: ")
+					fmt.Scanln(&add)
+					fmt.Print("length values:")
+					fmt.Scanln(&leng)
+
+					fmt.Print("Enter data values: ")
+					input.Scan()
+					err, _ := strconv.Atoi(input.Text())
+					arr = strings.Split(input.Text(), " ")
+						if err == 1 || err == 0  && leng == len(arr) && add < 100{
+							mbc.WriteCoils(1, add, arr)
+							Continue()
+							fmt.Scanln(&num)
+							if num == 1 {
+								continue OutputCoils
+							}else if num == 2{
+								continue Main
+							}
+						} else {
+							Scrclr()
+							Error()
+							fmt.Scanln(&num)
+							if num == 1{
+								continue OutputCoils
+							}else if num == 2{
+								continue Main
+							}
+						}
 				}
 				if num == 2{
-					fmt.Println("\n\n\n\n@@Output Coils@@\nRead Coils\n\n\n")
-					fmt.Print("\nalias values: ")
-					fmt.Scanln(&alias)
-					fmt.Print("leng values:")
+					Scrclr()
+					println("-------------------------------------")
+					fmt.Println("Read Coils")
+					println("-------------------------------------")
+					fmt.Print("\n\n\nStart address: ")
+					fmt.Scanln(&add)
+					fmt.Print("length values:")
 					fmt.Scanln(&leng)
-					data, _ := mbc.ReadCoil(1, alias, leng)
-					fmt.Println("Data values : ",data)
-					continue loop
-				}else{
-					fmt.Println("Go back")
-					continue loop
+
+					data, _ := mbc.ReadCoil(1, add, leng)
+					fmt.Println("Data values : ", data)
+					Continue()
+					fmt.Scanln(&num)
+					if num == 1 {
+						continue OutputCoils
+						} else if num == 2 {
+							continue Main
+						}
+					}
+				if num == 3{
+					continue Main
+				} else{
+					fmt.Println("Please enter again")
+					continue OutputCoils
 				}
 			}
 		}
 		if num == 2{
-
+			InputCoils:
 			for {
-				fmt.Print("\n\nno.1    : Read Coils Input\n","Any key : Go back\n\n\n")
-				fmt.Print("Select number Enter:")
+				Scrclr()
+				println("-------------------------------------")
+				fmt.Println("Input Coils")
+				println("-------------------------------------")
+				fmt.Print("\n\n\nStart address: ")
+				fmt.Scanln(&add)
+				fmt.Print("length values:")
+				fmt.Scanln(&leng)
+
+				data, _ := mbc.ReadCoilIn(1, add, leng)
+				fmt.Println("Data value:",data)
+				Continue()
 				fmt.Scanln(&num)
-				if num == 1{
-					fmt.Println("\n\n\n\n@@Input Coils@@\nRead Coils Input\n\n\n")
-					fmt.Print("\nalias values: ")
-					fmt.Scanln(&alias)
-					fmt.Print("leng values:")
-					fmt.Scanln(&leng)
-					data, _ := mbc.ReadCoilIn(1, alias, leng)
-					fmt.Println("Data value:",data,"\n\n")
-				}else{
-					fmt.Println("Go back")
-					continue loop
+				if num == 1 {
+					continue InputCoils
+				} else if num == 2 {
+					continue Main
 				}
 			}
 		}
 		if num == 3{
-
+			InputRegisters:
 			for {
-				fmt.Print("\n\nno.1    : Read Register Input\n","Any key : Go back\n\n\n")
-				fmt.Print("Select number Enter:")
+				Scrclr()
+				println("-------------------------------------")
+				fmt.Println("Input Registers")
+				println("-------------------------------------")
+				fmt.Print("\n\n\nStart address: ")
+				fmt.Scanln(&add)
+				fmt.Print("length values:")
+				fmt.Scanln(&leng)
+				data, _ := mbc.ReadRegIn(1, add, leng)
+				fmt.Println("Data values : ",data)
+				Continue()
 				fmt.Scanln(&num)
-				if num == 1{
-					fmt.Println("\n\n\n\n@@Input Registers@@\nRead Register Input\n\n\n")
-					fmt.Print("\nalias values: ")
-					fmt.Scanln(&alias)
-					fmt.Print("leng values:")
-					fmt.Scanln(&leng)
-					data, _ := mbc.ReadRegIn(1, alias, leng)
-					fmt.Println("Data values : ",data)
-				}else{
-					fmt.Println("Go back")
-					continue loop
+				if num == 1 {
+					continue InputRegisters
+				} else if num == 2 {
+					continue Main
 				}
 			}
 		}
 
 		if num == 4 {
-		loop4:
+		HoldingRegisters:
 			for {
-				fmt.Print("\n\nno.1    : Write Registers\n", "no.2    : Read Registers\n","Any key : Go back\n\n\n")
+
+				Scrclr()
+				println("-------------------------------------")
+				fmt.Println("Holding Registers")
+				println("-------------------------------------")
+				fmt.Print("1 : Write Registers\n","2 : Read Registers\n","3 : Go back\n\n\n")
 				fmt.Print("Select number Enter:")
 				fmt.Scanln(&num)
-
 				if num == 1 {
+					Scrclr()
 					var arr []string
-					fmt.Println("\n\n\n\n@@Holding Registers@@\nWrite Registers\n\n\n")
-					fmt.Print("\nalias values: ")
-					fmt.Scanln(&alias)
-					fmt.Print("leng values:")
+					var leng int
+					println("-------------------------------------")
+					fmt.Println("Write Registers")
+					println("-------------------------------------")
+					fmt.Print("\n\n\nStart address: ")
+					fmt.Scanln(&add)
+					fmt.Print("length values:")
 					fmt.Scanln(&leng)
-					for i := 0; i < int(leng); i++ {
-						fmt.Print("Enter Data values: ")
-						input.Scan()
-						err, _ := strconv.Atoi(input.Text())
+					fmt.Print("Enter Data values: ")
+					input.Scan()
+					err, _ := strconv.Atoi(input.Text())
 
-						if err > 6555 {
-							fmt.Println("max Excess error")
-							continue loop
+					if err > 65535 {
+						Scrclr()
+						fmt.Println("\n[Max Excess error]")
+						Error()
+						fmt.Scanln(&num)
+						if num == 1{
+							continue HoldingRegisters
+						}else if num == 2{
+							continue Main
 						}
-						arr = append(arr, input.Text())
-						mbc.WriteRegs(1, alias, arr)
 					}
+						arr = strings.Split(input.Text()," ")
+						//bbb := strconv.Itoa(leng)
+						if leng == len(arr){
+							mbc.WriteRegs(1, add, arr)
+							Continue()
+							fmt.Scanln(&num)
+							if num == 1 {
+								continue HoldingRegisters
+							} else if num == 2 {
+								continue Main
+							}
+						}else{
+							Scrclr()
+							fmt.Println("\n[Entered incorrectly length values]")
+							Error()
+							fmt.Scanln(&num)
+							if num == 1{
+								continue HoldingRegisters
+							}else if num == 2{
+								continue Main
+							}
+						}
+
 				}
 				if num == 2{
-					fmt.Println("\n\n\n\n@@Holding Registers@@\nRead Registers\n\n\n")
-					fmt.Print("\nalias 입력: ")
-					fmt.Scanln(&alias)
-					fmt.Print("leng 입력:")
+					println("-------------------------------------")
+					fmt.Println("Read Registers")
+					println("-------------------------------------")
+					fmt.Print("\n\n\nStart address: ")
+					fmt.Scanln(&add)
+					fmt.Print("length values:")
 					fmt.Scanln(&leng)
-					data, _ := mbc.ReadReg(1, alias, leng)
+					data, _ := mbc.ReadReg(1, add, leng)
 					fmt.Println("Data values : ",data)
-					continue loop4
-				}else{
-					fmt.Println("Go back")
-					continue loop
+					Continue()
+					fmt.Scanln(&num)
+					if num == 1 {
+						continue HoldingRegisters
+					} else if num == 2 {
+						continue Main
+					}
+				}
+				if num == 3{
+					continue Main
+				} else{
+					continue HoldingRegisters
 				}
 			}
 		}
 	}
 }
-
+	/*readData := make([]byte, 3)
+	  readData[0] = byte(200 >> 8)   // (High Byte)
+	  readData[1] = byte(200 & 0xff) // (Low Byte)
+	  readData[2] = 0x01
+	  Rx, rerr := TCPRead(m.Conn, 300, 1, modbusclient.FUNCTION_READ_HOLDING_REGISTERS, false, 0x00, readData, trace)
+	  if  rerr != nil{
+	          log.Println(rerr)
+	  }
+	  log.Println(Rx)*/
 
